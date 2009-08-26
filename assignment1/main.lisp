f1c9b61575dc2798cd05f0de34bffffa9ad7637d
Stephen Merity <smerity@phoenix.(none)>
2009-08-27 01:17:59 +1000

    Fixed up a few small errors and did some code cleaning in general

--- assignment1/main.lisp
+++ assignment1/main.lisp
@@ -1,22 +1,26 @@
-;#!/usr/bin/gcl -f
 
+(set 'operators '((s-eq . eq) (s-le . <) (s-ge . >) (s-leq . <=) (s-geq . >=) (s-diff . neq)))
+; Clean function for converting between operator terms
 (defun conv-string (str)
-	(cond
-	  ( (eql 's-eq str) 'eq)
-	  ( (eql 's-le str) '<)
-	  ( (eql 's-ge str) '>)
-	  ( (eql 's-leq str) '<=)
-	  ( (eql 's-geq str) '>=)
-	  ( (eql 's-diff str) 'neq)
-	)
+  (cdr (assoc str operators))
+)
+
+(defun and2 (test1 test2)
+  (and test1 test2)
+)
+
+(defun or2 (test1 test2)
+  (or test1 test2)
 )
 
 (defun find-columns (pred)
 	(cond 
 	  ( (eql 's-and (first pred)) (union ( find-columns (nth 1 pred) ) ( find-columns (nth 2 pred) ))) 
-	  ( (eql 's-or  (first pred)) (union ( find-columns (nth 1 pred) ) ( find-columns (nth 2 pred) ))) 
-	  ( (eql 's-not (first pred)) (find-colums rest(pred) ))
-	  ( T  (list (nth 1 pred)) )
+	  ( (eql 's-or  (first pred)) (union ( find-columns (nth 1 pred) ) ( find-columns (nth 2 pred) )))
+	  ; Spelling error on colum(n)s and rest(pred)
+	  ( (eql 's-not (first pred)) (find-columns (rest pred) ))
+	  ; Used nth 1 instead of nth 0 - but mayber first instead?
+	  ( T  (list (first pred)) )
 	)
 )
 
@@ -35,6 +39,7 @@
 			`(not ,(transformerR (rest query) ) )
 		)
 
+    ; Issue with the way this base case goes ...
 		( T 
 			`(,(conv-string (first query)) (nth 1 (assoc ',(nth 1 query) row)) ,(nth 2 query) )
 		)
@@ -48,13 +53,22 @@
 
 (setq tq (transformer '(s-and (s-geq B 5) (s-le B 100))))
 
-(format T "~a~%"
+;(mapcar #tq spreadsheet)
+;(mapcar #'(lambda (x) (and (#tq x) (list x))) spreadsheet)
+
+(format T "~a~%~%"
 	(list 
 		'(funcall tq  ( (A 300) (B 20) ) ) 
 		 (funcall tq '( (A 300) (B 20) ) )
 	)
 )
 
-
-
-
+(format T "Test - should be 'nil T nil'~%=-=-=-=-=~%")
+(setq q (transformer '(s-and (s-eq A 6)(s-ge B 100))))
+(format T "~a~%" 
+  (list 
+    (funcall q '( (A 7)(B 6)(C 9) ) )
+    (funcall q '( (A 6)(B 101) ) )
+    '(funcall q '( (C 6) ) )
+  )
+)
