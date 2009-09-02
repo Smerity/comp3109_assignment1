8f5e77cd7bc55c3050d6e27c21309821666178a8
Stephen Merity <smerity@phoenix.(none)>
2009-09-02 21:23:03 +1000

    Cleaning code

--- assignment1/main.lisp
+++ assignment1/main.lisp
@@ -20,20 +20,18 @@
   (nilp x)
 )
 
-; define a not equal function because lisp does not recognise neq
+; Define a not equal function because Lisp does not recognise neq
 (defun neq (x y)
-  ; Maybe I should be doing eql = "eq + =", where eq is true if same symbol
   not (eql x y)
 )
 
-; part 1:
-;  this function has a series of conds which recursivly finds the union of all the columns in the query
+; Part 1:
+;  This function has a series of conds which recursivly finds the union of all the columns in the query
 (defun find-columns (pred)
 	(cond 
-	  ; Recurse for AND OR and NOT
+	  ; Recurse for AND, OR and NOT
 	  ( (eql 's-and (first pred)) (union ( find-columns (nth 1 pred) ) ( find-columns (nth 2 pred) ))) 
 	  ( (eql 's-or  (first pred)) (union ( find-columns (nth 1 pred) ) ( find-columns (nth 2 pred) )))
-	  ; added nth 1 because rest pred, is a list
 	  ( (eql 's-not (first pred)) (find-columns (nth 1 pred)))
 	  
 	  ; The base case, when we are left with a single predicate return the column letter
@@ -41,17 +39,17 @@
 	)
 )
 
-; Part 2: this is the section where the function is built.
-;
+; Part 2:
+; This is the section where the function is built.
+; 
 ; The transformerR function is the recursive function that builds the main function.
 (defun transformerR (query)
 	(cond
-		; Recurse for AND OR and NOT cases.
+		; Recurse for AND, OR and NOT cases.
 		( (eq 's-and (first query) )
-			; use our and2 function along with the results from the 2 recursive calls
+			; Use our and2 function along with the results from the 2 recursive calls
 			`(and2 ,(transformerR (nth 1 query)) ,(transformerR (nth 2 query)) ) 
 		)
-		
 		( (eq 's-or (first query) ) 
 			`(or2 ,(transformerR (nth 1 query)) ,(transformerR (nth 2 query)) ) 
 		)
@@ -62,11 +60,11 @@
 		)
 		; The base case, recurse if its not in the AND OR or NOT case
 		( T
-			;if the row we are looking for does not exist, return null this will make
-			;AND not fail and make OR succeed when the 2nd argument has a non existant column
+			;If the row we are looking for does not exist, return null. This will make
+			;AND not fail and make OR succeed when the 2nd argument has a non existent column
 		   `(if (null (assoc ',(nth 1 query) row)) 
 			     () 
-				 ; build the lisp function that relates to the criteria we are processing
+				 ; Build the Lisp function that relates to the criteria we are processing
 				 (,(conv-string (first query)) (nth 1 (assoc ',(nth 1 query) row)) ,(nth 2 query) )
 			)
 		)
@@ -79,28 +77,3 @@
 	;(format T "~a~%" query)
 	(eval `(lambda(row) ,( transformerR query) ))
 )
-
-;(load 'testcases1.l')
-;(load 'testcases2.l')
-;(setq tq (transformer '(s-and (s-geq B 5) (s-le B 100))))
-
-;(mapcar #tq spreadsheet)
-;(mapcar #'(lambda (x) (and (#tq x) (list x))) spreadsheet)
-
-;(format T "~a~%~%"
-;	(list 
-;		'(funcall tq  ( (A 300) (B 20) ) ) 
-;		 (funcall tq '( (A 300) (B 20) ) )
-;	)
-;)
-
-;(format T "Test - should be 'nil T nil'~%=-=-=-=-=~%")
-;(setq q (transformer '(s-and (s-eq A 6)(s-ge B 100))))
-;(format T "~a~%" 
-;  (list 
-;    (funcall q '( (A 7)(B 6)(C 9) ) )
-;    (funcall q '( (A 6)(B 101) ) )
-;    (funcall q '( (C 6) ) )
-;    (funcall q '((Z 315) (B 324) (C 528) (D 618) (E 423) (F 275) (G 768) (H 689) (I 663) (J 266) (K 985) (L 94) (M 731) (N 903) (O 544) (P 702) (Q 558) (R 52) (S 34) (T 456) (U 43) (V 421) (W 97) (X 73) (Y 940) (A 756)) )
-;  )
-;)
